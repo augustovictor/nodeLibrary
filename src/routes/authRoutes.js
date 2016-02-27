8d9eb2b38691e2e4185e2ac61fcc38243c4d1ed8
Victor <victoraweb@gmail.com>
2016-02-27 10:33:54 -0300

    Adding user to database

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,13 +3,25 @@ var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
 var router = function () {
-
+    'use strict';
     authRouter.route('/signup')
         .post(function (req, res) {
             console.log(req.body);
-            req.login(req.body, function () {
-                res.redirect('/auth/profile');
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users'); // If we do not have a collection called 'users' mongodb will created
+                var user = {
+                    username: req.body.username, // The names html form inputs
+                    password: req.body.password
+                };
+                collection.insert(user, function (err, result) {
+                    req.login(result.ops[0], function () {
+                        res.redirect('/auth/profile');
+                    });
+
+                });
             });
+
         });
     authRouter.route('/profile')
         .get(function (req, res) {
@@ -17,6 +29,6 @@ var router = function () {
         });
 
     return authRouter;
-}
+};
 
 module.exports = router;
